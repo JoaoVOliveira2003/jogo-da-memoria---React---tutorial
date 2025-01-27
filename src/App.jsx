b9e41ae106d4849f3321ab70a31c3a22f43447c4
JoaoVOliveira2003 <ojoao953@gmail.com>
2025-01-27 16:54:46 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -4,109 +4,106 @@ import MemoryCard from "./components/MemoryCard";
 
 
 export default function App() {
-  const [isGameOn, setIsGameOn]           = useState(false)
-  const [emojisData, setEmojisData]       = useState([])
-  const [selectedCards, setSelectedCards] = useState([])
-  const [matchedCards, setmatchedCards]   = useState([])
-  const [isGameOver,setIsGameOver]        = useState(false)
-
-  console.log(selectedCards);
-
-  useEffect(() => {
-    if(selectedCards.length === 2 && selectedCards[0].name === selectedCards[1].name){
-      setmatchedCards(prevMatchedCards => [...prevMatchedCards, ...selectedCards])
+    const [isGameOn, setIsGameOn] = useState(false)
+    const [emojisData, setEmojisData] = useState([])
+    const [selectedCards, setSelectedCards] = useState([])
+    const [matchedCards, setMatchedCards] = useState([])
+    const [isGameOver, setIsGameOver] = useState(false)
+    
+    useEffect(() => {
+        if (selectedCards.length === 2 && selectedCards[0].name === selectedCards[1].name) {
+            setMatchedCards(prevMatchedCards => [...prevMatchedCards, ...selectedCards])
+        }
+    }, [selectedCards])
+    
+    useEffect(() => {
+        if (emojisData.length && matchedCards.length === emojisData.length) {
+            setIsGameOver(true)
+        }
+    }, [matchedCards])
+    
+    async function startGame(e) {
+        e.preventDefault()
+        
+        try {
+            const response = await fetch("https://emojihub.yurace.pro/api/all/category/animals-and-nature")
+            
+            if (!response.ok) {
+                throw new Error("Could not fetch data from API")
+            }
+            
+            const data = await response.json()
+            const dataSlice = await getDataSlice(data)
+            const emojisArray = await getEmojisArray(dataSlice)
+            
+            setEmojisData(emojisArray)
+            setIsGameOn(true)
+        } catch(err) {
+            console.error(err)
+        }   
     }
-  }, [selectedCards] )
 
-  useEffect(() => {
-    if(emojisData.length && matchedCards.length === emojisData.length){
-        setIsGameOver(true)
+    async function getDataSlice(data) {
+        const randomIndices = getRandomIndices(data)
+        
+        const dataSlice = randomIndices.reduce((array, index) => {
+            array.push(data[index])
+            return array
+        }, [])
+
+        return dataSlice
     }
-  },[matchedCards])
 
+    function getRandomIndices(data) {        
+        const randomIndicesArray = []
+ 
+        for (let i = 0; i < 5; i++) {
+            const randomNum = Math.floor(Math.random() * data.length)
+            if (!randomIndicesArray.includes(randomNum)) {
+                randomIndicesArray.push(randomNum)
+            } else {
+                i--
+            }
+        }
+        
+        return randomIndicesArray
+    }
 
-  async function startGame(e) {
-      e.preventDefault()
-      
-      try {
-          const response = await fetch("https://emojihub.yurace.pro/api/all/category/animals-and-nature")
-          
-          if (!response.ok) {
-              throw new Error("Could not fetch data from API")
-          }
-          
-          const data = await response.json()
-          const dataSlice = await getDataSlice(data)
-          const emojisArray = await getEmojisArray(dataSlice)
-          
-          setEmojisData(emojisArray)
-          setIsGameOn(true)
-      } catch(err) {
-          console.error(err)
-      }   
-  }
-
-  async function getDataSlice(data) {
-      const randomIndices = getRandomIndices(data)
-      
-      const dataSlice = randomIndices.reduce((array, index) => {
-          array.push(data[index])
-          return array
-      }, [])
-
-      return dataSlice
-  }
-
-  function getRandomIndices(data) {        
-      const randomIndicesArray = []
-
-      for (let i = 0; i < 5; i++) {
-          const randomNum = Math.floor(Math.random() * data.length)
-          if (!randomIndicesArray.includes(randomNum)) {
-              randomIndicesArray.push(randomNum)
-          } else {
-              i--
-          }
-      }
-      
-      return randomIndicesArray
-  }
-
-  async function getEmojisArray(data) {
-      const pairedEmojisArray = [...data, ...data]
-      
-      for (let i = pairedEmojisArray.length - 1; i > 0; i--) {
-          const j = Math.floor(Math.random() * (i + 1))
-          const temp = pairedEmojisArray[i]
-          pairedEmojisArray[i] = pairedEmojisArray[j]
-          pairedEmojisArray[j] = temp
-      }
-      
-      return pairedEmojisArray
-  }
-   
-  
-  function turnCard(name, index) {
-      const selectedCardEntry = selectedCards.find(emoji => emoji.index === index)
-      
-      if (!selectedCardEntry && selectedCards.length < 2) {
-          setSelectedCards(prevSelectedCards => [...prevSelectedCards, { name, index }])
-      } else if (!selectedCardEntry && selectedCards.length === 2) {
-          setSelectedCards([{ name, index }])
-      }
-  }
-   
-  return (
-      <main>
-          <h1>Jogo da memoria</h1>
-          {!isGameOn && <Form handleSubmit={startGame} />}
-          {isGameOn &&
-          <MemoryCard 
-            handleClick={turnCard}
-            data={emojisData}
-            selectedCards={selectedCards}
-            matchedCards ={matchedCards}
-          />}
-      </main>
-  )
+    async function getEmojisArray(data) {
+        const pairedEmojisArray = [...data, ...data]
+        
+        for (let i = pairedEmojisArray.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1))
+            const temp = pairedEmojisArray[i]
+            pairedEmojisArray[i] = pairedEmojisArray[j]
+            pairedEmojisArray[j] = temp
+        }
+        
+        return pairedEmojisArray
+    }
+    
+    function turnCard(name, index) {
+        console.log("Clicked")
+        
+        if (selectedCards.length < 2) {
+            setSelectedCards(prevSelectedCards => [...prevSelectedCards, { name, index }])
+        } else if (selectedCards.length === 2) {
+            setSelectedCards([{ name, index }])
+        }
+    }
+    
+    return (
+        <main>
+            <h1>Memory</h1>
+            {!isGameOn && <Form handleSubmit={startGame} />}
+            {isGameOn &&
+                <MemoryCard
+                    handleClick={turnCard}
+                    data={emojisData}
+                    selectedCards={selectedCards}
+                    matchedCards={matchedCards}
+                />
+            }
+        </main>
+    )
 }
--- src/components/EmojiButton.jsx
+++ src/components/EmojiButton.jsx
@@ -6,18 +6,17 @@ export default function EmojiButton({
 }) {
     
     const btnContent = selectedCardEntry || matchedCardEntry ? content : "?"
-
-
     
     const btnStyle =
         matchedCardEntry ? "btn--emoji__back--matched" :
         selectedCardEntry ? "btn--emoji__back--selected" :
         "btn--emoji__front"
- 
+
     return (
         <button
             className={`btn btn--emoji ${btnStyle}`}
-            onClick={handleClick}
+            onClick={selectedCardEntry ? null : handleClick}
+            disabled={matchedCardEntry}
         >
             {btnContent}
         </button>
